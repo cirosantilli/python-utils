0dea141c2518ab467b8e0af0e80d4a9a32a9650e
Ciro Duran Santillli <ciro.santilli@gmail.com>
2013-05-06 12:10:27 +0200

    setup tests added and improved, improved matplotlib makefile

--- setup.py
+++ setup.py
@@ -1,76 +1,149 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-#from setuptools import setup #this is distutils, which is a currently remerging fork of setuptools....
+"""
+this is the central cheat for the various setup tools like ``distutils`` or ``setuptools``
 
-#you *need* the files:
+#you need the files
 
-#MANIFEST.txt
-#CHANGES.txt
+- MANIFEST.txt
+ CHANGES.txt
 
-#or it won't work!
+or it won't work!
 
-##which tool to use to distribute?
+#which tool to use to distribute?
 
-#python distribution is currently messy
+python distribution is currently messy
 
-#see:
+see:
 
-#- http://stackoverflow.com/questions/6344076/differences-between-distribute-distutils-setuptools-and-distutils2
+- http://stackoverflow.com/questions/6344076/differences-between-distribute-distutils-setuptools-and-distutils2
+- http://python-notes.boredomandlaziness.org/en/latest/pep_ideas/core_packaging_api.html
 
-#- http://python-notes.boredomandlaziness.org/en/latest/pep_ideas/core_packaging_api.html
+current best course of action for python only projects:
 
-#current best course of action for python only projects:
+- non stdlib `distribute` to create packages.
+   Note that the `distribute` base module is called `setuptools` because `distribute` is a fork of `setuptools`...
+- pypi to host the packages: https://pypi.python.org/pypi
+- `pip` to install packages from pypi automatically
 
-#- non stdlib `distribute` to create packages.
-    #Note that the `distribute` base module is called `setuptools` because `distribute` is a fork of `setuptools`...
-#- pypi to host the packages: https://pypi.python.org/pypi
-#- `pip` to install packages from pypi automatically
+the best you can do with the stdlib is `distutils`,
+but this is worse than `distribute`.
 
-#the best you can do with the stdlib is `distutils`,
-#but this is worse than `distribute`.
+#distribute
 
-##usage
+is seems to be backwards compatible with ``distutils`` TODO check
 
-#the setup function will parse command line arguments and can install everything if you tell it to.
+##install and uninstall
 
-#**however** there is no automatic way to uninstall!!....
-#<http://stackoverflow.com/questions/402359/how-do-you-uninstall-a-python-package-that-was-installed-using-distutils>
+the setup function will parse command line arguments and can install everything if you tell it to.
 
-#you should use a packag manger like `pip` for that TODO how
+basic install:
 
-#the best you can currently do without a package manger is:
+    sudo python setup.py install
 
-    #sudo python setup.py install --record record.txt
+this
 
-#so that record.txt will contain the installed files, so to uninstall you can:
+- moves files to the correct install location
+- overwrites any existing files updating them.
+- creates a build dir in current dir which you should ignore in your gitignore
 
-    #cat record.txt | xargs sudo rm -rf
+**however** there is no automatic way to uninstall!!....
+<http://stackoverflow.com/questions/402359/how-do-you-uninstall-a-python-package-that-was-installed-using-distutils>
 
-#clearly a hack =)
+you should use a packag manger like `pip` for that TODO how
+
+the best you can currently do without a package manger is:
+
+   sudo python setup.py install --record record.txt
+
+so that record.txt will contain the installed files, so to uninstall you can:
+
+   cat record.txt | xargs sudo rm -rf
+
+clearly a hack =)
+
+##develop
+
+   sudo python setup.py develop
+
+only installs executables in path, but keeps python modules in place
+so that you can edit them where they are for tests
+
+##test
+
+TODO
+
+#egg
+
+TODO what is
+"""
+
+from distutils.core import setup   #only for basic projects
+#from setuptools import setup        #this is distutils, which is a currently remerging fork of setuptools....
 
 setup(
-    name='cirosantilli',
-    version='0.0.1',
-    author='Ciro Duran Santilli',
-    author_email='ciro.santilli@gmail.com',
+    name                = 'cirosantilli',
+    version             = '0.0.1',
+    author              = 'Ciro Duran Santilli',
+    author_email        = 'ciro.santilli@gmail.com',
+    url                 = 'https://github.com/cirosantilli/',
+    license             = 'license.md', #GPL, BSD, or MIT. firefox http://www.codinghorror.com/blog/2007/04/pick-a-license-any-license.html 
+    description         = 'my simple python scripts and modules',
+    long_description    = open('readme.md').read(),
 
-    #whatever is listed here will be put in your your pythonpath: (`/usr/local/lib/python2.7/dist-packages/` for ubuntu)
-    packages=['cirosantilli'],
+    ##packages
 
-    #whatever is listed here will be put in your bin path (`/usr/local/bin` on current ubuntu):
-    scripts=[
-        'bin/move_regex.py',   
+    #whatever package (dir with ``__init__.py`` and everything under)is listed
+    #here will be put in your your pythonpath: #(`/usr/local/lib/python2.7/dist-packages/` for ubuntu):
+
+    packages = [
+        'cirosantilli',
+        'setup_test_dir',
     ],
 
-    url='https://github.com/cirosantilli/',
-    license='license.md', #GPL, BSD, or MIT. firefox http://www.codinghorror.com/blog/2007/04/pick-a-license-any-license.html 
-    description='my simple python scripts and modules',
-    long_description=open('readme.md').read(),
+    #only python files are copyied.
+
+    #if you want to add data files your package, use [package data]
+
+    ##package data
+
+    package_data = {
+        #'setup_test_dir': ['*.txt']
+        #'setup_test_dir': ['*.dat']
+    },
+
+    ##package_dir
+
+    #specifies where packages will be put
+
+    #all packages are under ``./lib/``:
+
+        #package_dir = {'': 'lib'},
+
+    #pac package is under ``./lib/``:
+
+        #package_dir = {
+            #'pac': 'lib'
+        #},
+
+    ##py_modules
+
+    #specify individual modules (``.py`` or a dir with ``__init__.py``, but not all of its contents! )
+    py_modules = [
+        #'setup_test',
+        #'setup_test_dir.setup_test2',
+    ],
+
+    ##scripts
+
+    #whatever is listed here will be put in your bin path (`/usr/local/bin` on current ubuntu):
+    scripts = [
+        #'bin/move_regex.py',
+    ],
 
     #whatever is listed here will be installed if not already:
-    install_requires=[
+    install_requires = [
 
         #current best packaging tool (non stdlib):
         "distribute",
@@ -95,5 +168,18 @@ setup(
         #TODO vs pip freeze. I htink this looks under installation not managed by pip
         "yolk",
     ],
-    #* failed for be, better with distro's package manager
+    #* failed for this packagebe, better with distro's package manager
+
+    ##data_files
+
+    #system independent data files.
+
+    #relative paths go under `sys.prefix`, which equals `/usr/` in current Ubuntu for example.
+
+    #basenames cannot be changed
+
+    data_files = [
+        #('bitmaps', ['bm/b1.gif', 'bm/b2.gif']),   #files will go under `sys.prefix + bitmaps`
+        #('/etc/init.d', ['init-script'])           #files will go under `/etc/init.d/`
+    ]
 )
---
+++ setup_test.py
@@ -0,0 +1,5 @@
+"""
+the only purpose of this file is to test where it will go after a ./setup.py install
+"""
+
+a = 10
